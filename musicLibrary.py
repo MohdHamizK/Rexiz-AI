b3173325a5cf71d2844eb0b0adfd219f16f91c41
MohdHamizK <kattekarhamiz25@gmail.com>
2025-04-30 01:44:24 +0530

    My Rexiz Project

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "sky": "https://www.youtube.com/watch?v=U47Tr9BB_wE", 
+    "fat": "https://www.youtube.com/watch?v=3fxq7kqyWO8",
+    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw"
+}
